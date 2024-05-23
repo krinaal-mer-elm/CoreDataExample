ff4c92f0ea329277aebf0d3c086ae3e7c5bccd6b
krinaal-mer-elm <krinal.elluminatiinc@gmail.com>
2024-05-23 10:21:34 +0530

    Initial Commit

---
+++ CoreDataExample/CoreDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleApp.swift
+//  CoreDataExample
+//
+//  Created by elluminati on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
